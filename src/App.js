61fb8c2291edd271757ccba81fc6cea4e1248c0b
Michael-Otieno <michael.otieno@student.moringaschool.com>
2022-06-16 18:05:52 +0300

    add singup component and connect to App.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@
 import './App.css';
 import {Routes, Route} from 'react-router-dom';
 import Homepage from './mcomponents/homepage/Homepage';
+import Signup from './mcomponents/signup/Signup';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path='/' element={<Homepage />} />
-        <Route path='/signup' element={<div>Signup</div>} />
+        <Route path='/signup' element={<Signup />} />
       </Routes>
       
     </div>
