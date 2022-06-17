8601b084170293f9f69401ee3757b4a84f69edd3
Michael-Otieno <michael.otieno@student.moringaschool.com>
2022-06-17 07:09:58 +0300

    add login details and styles

--- src/mcomponents/login/Login.js
+++ src/mcomponents/login/Login.js
@@ -1,11 +1,41 @@
+import { Link } from "react-router-dom";
 
-import './Login.css';
+// import errorIcon from "../../imgs/error.png";
+import nextIcon from "../../imgs/next.png";
+import signupIcon from "../../imgs/signup.png";
 
-function Login(){
+import "./Login.css";
+
+function Login() {
   return (
-    <div>
-      Login
+    <div className="login">
+      <div className="login-div">
+        <div>
+          <h2>Sign in</h2>
+          <form>
+            <input type="email" placeholder="email" />
+            <br />
+            <input type="password" placeholder="password" />
+            <br />
+
+            {/* <p className="error-icon-para">
+            <img className="error-icon" src={errorIcon} alt="" />
+            <span>reply with your correct company name</span>
+          </p> */}
+          </form>
+
+          <button className="nextIcon">
+            <Link to="/login">
+              <img className="next-btn" alt="next button" src={nextIcon} />
+            </Link>
+          </button>
+        </div>
+      </div>
+
+      <div className="e-tech-img">
+        <img className="login-img" src={signupIcon} alt="login up" />
+      </div>
     </div>
-  )
+  );
 }
 export default Login;
