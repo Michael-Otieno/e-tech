688fe8c9ab6e82328d4ef8dbab64cf74daab0822
Michael-Otieno <michael.otieno@student.moringaschool.com>
2022-06-17 15:23:16 +0300

    add userdetails component and styles

--- src/mcomponents/dashboard/Dashboard.js
+++ src/mcomponents/dashboard/Dashboard.js
@@ -1,17 +1,21 @@
-
 import "./Dashboard.css";
 
-
-import Sidebar from "../sidebar/Sidebar"; 
-
+import Sidebar from "../sidebar/Sidebar";
+import Userdetails from "../userdetails/Userdetails";
 
 function Dashboard() {
   return (
     <div className="dashboard">
-      
       <Sidebar />
       <div className="content">
-        <h2>My Recent Content</h2>
+        <div className="content-header">
+          <h2>My Recent Content</h2>
+          <Userdetails/>
+        </div>
+
+        <p>
+          lorem 10
+        </p>
       </div>
     </div>
   );
---
+++ src/mcomponents/userdetails/Userdetails.js
@@ -0,0 +1,13 @@
+import "./Userdetails.css";
+
+function Userdetails() {
+  return (
+    <div className="user-details">
+      <h3>Company name</h3>
+      <img src="" alt="user" />
+      <p>Anonymous</p>
+    </div>
+  );
+}
+
+export default Userdetails;
