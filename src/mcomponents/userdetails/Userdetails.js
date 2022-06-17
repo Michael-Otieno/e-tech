adce4d70d78cb3172e798b8fce7a11228922c90b
Michael-Otieno <michael.otieno@student.moringaschool.com>
2022-06-17 17:49:45 +0300

    add dashboard contents and styles

--- src/mcomponents/userdetails/Userdetails.js
+++ src/mcomponents/userdetails/Userdetails.js
@@ -1,10 +1,12 @@
 import "./Userdetails.css";
+import tests from "../../imgs/tests.png";
 
-function Userdetails() {
+
+function Userdetails(){
   return (
     <div className="user-details">
       <h3>Company name</h3>
-      <img src="" alt="user" />
+      <img className="user-icon" src={tests} alt="user" />
       <p>Anonymous</p>
     </div>
   );
