2ede520b4230b735c0d015722a96a0ab001321a2
Michael-Otieno <michael.otieno@student.moringaschool.com>
2022-06-17 06:14:53 +0300

    add styles to signup component

--- src/mcomponents/homepage/Homepage.js
+++ src/mcomponents/homepage/Homepage.js
@@ -7,7 +7,7 @@ function Homepage() {
   return (
     <div>
       <nav className="navbar">
-        <img src={logo} />
+        <img alt="" src={logo} />
         <ul>
           <li>Home</li>
           <li>What we do</li>
@@ -15,15 +15,15 @@ function Homepage() {
         </ul>
       </nav>
       <div className="digital">
-        <p>
+        <h1>
           Training your employees made digital and interactive. Try out our
           website today for more amazing features and awesome content.
           <br />
           <Link to='/signup'>
             <button>Get started</button>
           </Link>
-        </p>
-        <img src={edigital} />
+        </h1>
+        <img alt="" src={edigital} />
       </div>
     </div>
   );
