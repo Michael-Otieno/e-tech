d6f1f9a20637419551f6da00c55bd49944e7aab4
Michael-Otieno <michael.otieno@student.moringaschool.com>
2022-06-23 08:15:12 +0300

    add content details and styles

--- src/mcomponents/content/Content.js
+++ src/mcomponents/content/Content.js
@@ -2,12 +2,14 @@ import "./Content.css";
 
 import Sidebar from "../sidebar/Sidebar";
 import Userdetails from "../userdetails/Userdetails";
-import complete from "../../imgs/complete.png";
-import group from "../../imgs/group.png";
-import company from "../../imgs/company.png";
+import complete from "../../imgs/techent.jpg";
+import group from "../../imgs/group.jpg";
+import company from "../../imgs/company.jpg";
 import insurance from "../../imgs/insurance.png";
 import news from "../../imgs/news.png";
 import mental from "../../imgs/mental.png";
+
+import family from "../../imgs/family.jpg";
 
 import { Link } from "react-router-dom";
 
@@ -30,8 +32,8 @@ function Content() {
               </Link>
             </div>
             <h4>
-              Entrepreneurship 50%
-              <span>complete</span>
+              Entrepreneurship
+              {/* <span>complete</span> */}
             </h4>
           </div>
 
@@ -42,8 +44,8 @@ function Content() {
               </Link>
             </div>
             <h4>
-              Trainings 20%
-              <span>complete</span>
+              Start ups 
+              {/* <span>complete</span> */}
             </h4>
           </div>
 
@@ -54,8 +56,8 @@ function Content() {
               </Link>
             </div>
             <h4>
-              Leadership 80%
-              <span>complete</span>
+              Leadership
+              {/* <span>complete</span> */}
             </h4>
           </div>
 
@@ -66,20 +68,41 @@ function Content() {
               </Link>
             </div>
             <h4>
-              Tech Entrepreneurship 0%
-              <span>complete</span>
+              Tech Entrepreneurship
+            </h4>
+          </div>
+
+          <div className="content-grid">
+            <div className="content-grid-img">
+              <Link to="">
+              <img className="tech-ent" src={family} alt="" />
+              </Link>
+            </div>
+            <h4>
+              Family and Mental Health
+            </h4>
+          </div>
+
+          <div className="content-grid">
+            <div className="content-grid-img">
+              <Link to="">
+              <img className="tech-ent" src={insurance} alt="" />
+              </Link>
+            </div>
+            <h4>
+              Tech Entrepreneurship
             </h4>
           </div>
         </div>
 
-        <div className="news">
+        {/* <div className="news">
         <div className="content-grid-img">
             <img src={mental} alt="" />
             <h4>Family & Mental Health</h4>
           </div>
           <img width="50%" src={news} alt="" />
           
-        </div>
+        </div> */}
       </div>
     </div>
   );
