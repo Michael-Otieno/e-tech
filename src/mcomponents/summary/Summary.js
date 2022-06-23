1bdef4605431d0897b1c24b95a6d9670d42b4fc6
Michael-Otieno <michael.otieno@student.moringaschool.com>
2022-06-23 19:12:28 +0300

    add sidebar and navbar to summary component

--- src/mcomponents/summary/Summary.js
+++ src/mcomponents/summary/Summary.js
@@ -1,10 +1,27 @@
 import './Summary.css';
 
+import Sidebar from "../sidebar/Sidebar";
+import Userdetails from "../userdetails/Userdetails";
+
+
+
+
 function Summary(){
   return (
-    <div>
-      Kuome
+    <div className="dashboard">
+    <Sidebar />
+    <div className="content">
+      <div className="content-header">
+        <h2></h2>
+        <Userdetails />
+      </div>
+
+      <div className="recent-activities-history">
+          
+        </div>
+
     </div>
+  </div>
   )
 }
 
