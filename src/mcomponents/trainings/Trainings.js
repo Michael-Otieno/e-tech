5d21a757aa9d6f4ef745cf76580729fa7dc79852
Michael-Otieno <michael.otieno@student.moringaschool.com>
2022-06-23 16:15:35 +0300

    add trainings component details

--- src/mcomponents/trainings/Trainings.js
+++ src/mcomponents/trainings/Trainings.js
@@ -1,25 +1,67 @@
-import './Trainings.css';
+import "./Trainings.css";
 
 import Sidebar from "../sidebar/Sidebar";
 import Userdetails from "../userdetails/Userdetails";
 
-
-function Trainings(){
+function Trainings() {
   return (
     <div className="dashboard">
-    <Sidebar />
-    <div className="content">
-      <div className="content-header">
-        <h2></h2>
-        <Userdetails />
-      </div>
+      <Sidebar />
+      <div className="content">
+        <div className="content-header">
+          <h2></h2>
+          <Userdetails />
+        </div>
 
-      <div className="recent-activities-history">
-        <div></div>
-      </div>
+        <div className="trainings">
+          <div className="all-trainings">
+            <div className="company-training">
+              <h3>Company Trainings</h3>
+              <ul>
+                <li>
+                  <span>Management training by vision consultants</span><br/>
+                  <span className="time">Start: 9:00pm - Stop: 10:00</span>
+                </li>
+              </ul>
+            </div>
 
+            <div className="company-training">
+              <h3>General Trainings</h3>
+              <ul>
+                <li>
+                  <span>Management training by vision consultants</span><br/>
+                  <span className="time">Start: 9:00pm - Stop: 10:00</span>
+                </li>
+                <li>
+                  <span>Management training by vision consultants</span><br/>
+                  <span className="time">Start: 9:00pm - Stop: 10:00</span>
+                </li>
+              </ul>
+            </div>
+
+            <div className="ended-training">
+              <h3>Trainings Ended</h3>
+              <ul>
+                <li>
+                  <span>Management training by vision consultants</span><br/>
+                  <span className="time-end">Started: 9:00pm - Stop: 10:00</span>
+                </li>
+
+                <li>
+                  <span>Management training by vision consultants</span><br/>
+                  <span className="time-end">Started: 8:00pm - Stop: 9:00pm</span>
+                </li>
+
+                <li>
+                  <span>Management training by vision consultants</span><br/>
+                  <span className="time-end">Started: 4:00pm - Stop: 5:00pm</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
-  )
+  );
 }
 export default Trainings;
