6b66a6b5a4a5e59c6e1a127f29f94dde97904f67
Michael-Otieno <michael.otieno@student.moringaschool.com>
2022-06-27 13:11:44 +0300

    add line c graph

--- src/mcomponents/summary/Summary.js
+++ src/mcomponents/summary/Summary.js
@@ -1,76 +1,86 @@
-import './Summary.css';
+import "./Summary.css";
 
 import Sidebar from "../sidebar/Sidebar";
 import Userdetails from "../userdetails/Userdetails";
 
-import {Bar} from 'react-chartjs-2';
-import   {useState} from 'react';
+import { Line } from "react-chartjs-2";
+import { useState } from "react";
 
 import { Chart as ChartJS } from "chart.js/auto";
-import {saveAs} from 'file-saver';
+import { saveAs } from "file-saver";
 
-function Summary(){
-
-
+function Summary() {
   const [data, setData] = useState({
-    labels: ['Content', 'Trainings', 'Certifications',
-             ],
+    labels: ["","Content", "Trainings", "Certification",'Feedback'],
     datasets: [
       {
-        label: '',
-        backgroundColor: '#8A1A49',
-        borderColor: 'rgba(0,0,0,1)',
-        borderWidth: 2,
-        data: [65, 59, 80, 81, 56]
-      }
-    ]
-  })
+        label: "Check badge meaning for performance rating",
+        backgroundColor: "#8A1A49",
+        borderColor: "#DF5753",
+        borderWidth: 1,
+        data: [0,50,30,10,30,100],
+      },
+    ],
+  });
 
-
-  
   return (
     <div className="dashboard">
-    <Sidebar />
-    <div className="content">
-      <div className="content-header">
-        <h2></h2>
-        <Userdetails />
-      </div>
-
-      <div className="summary" id='summary'>
-          <div className='summary-back'>
-            <div className='graph'>
-            <Bar
-            data={data}
-            options={{
-              title:{
-                display:true,
-                text:'Performance',
-                fontSize:20
-              },
-              legend:{
-                display:true,
-                position:'right'
-              }
-            }}/>
-            </div>
-            <div className='graph-para'>
-              <h4>Overall Remarks</h4>
-              <p>
-                I'm trying to download my chart.js charts as png using a button Onclick, but I have no idea how I'm going to achieve this , I've went through this answer React-chartjs-2 Doughnut chart export to png but it wasn't quite clear enough for me as I'm quite new in chart.js don't know how I'm going to connect those variables with my button.
-              </p>
-
-              <h4>Employee</h4>
-              <p>
-                I'm trying to download my chart.js charts as png using a button Onclick, but I have no idea how I'm going to achieve this , I've went through this answer React-chartjs-2 Doughnut chart export to png but it wasn't quite clear enough for me as I'm quite new in chart.js don't know how I'm going to connect those variables with my button.
-              </p>
+      <Sidebar />
+      <div className="content">
+        <div className="content-header">
+          <h2></h2>
+          <Userdetails />
+        </div>
+        <div className="summary">
+          <div className="summary-graph">
+            {/* <div className="summary-back"> */}
+              <div className="graph">
+                <Line
+                  data={data}
+                  options={{
+                    title: {
+                      display: true,
+                      text: "Performance",
+                      fontSize: 20,
+                    },
+                    legend: {
+                      display: true,
+                      position: "right",
+                    },
+                  }}
+                />
+              </div>
+            {/* </div> */}
+            <div className="abilities">
+              <h6>Your overall abilities</h6>
+              <p></p>
             </div>
           </div>
-      </div>
 
+          <div>
+            <div>
+              <h6>Badge</h6>
+              <ul>
+                <li>Average:</li>
+                <li>Good:</li>
+                <li>Exceptional</li>
+              </ul>
+            </div>
+            <div>
+              <h6>BADGE</h6>
+              <ul>
+                <li>Content</li>
+                <li>Trainings</li>
+                <li>Certification</li>
+                <li>Feedback</li>
+                <li>Overall Badge</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
-  )
+  );
 }
 
 export default Summary;
