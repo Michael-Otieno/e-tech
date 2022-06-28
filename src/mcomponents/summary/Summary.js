ccffd145778d069a3c5a146aaececcaa842d3918
Michael-Otieno <michael.otieno@student.moringaschool.com>
2022-06-29 00:28:34 +0300

    add background color to graph

--- src/mcomponents/summary/Summary.js
+++ src/mcomponents/summary/Summary.js
@@ -9,16 +9,20 @@ import { useState } from "react";
 import { Chart as ChartJS } from "chart.js/auto";
 import { saveAs } from "file-saver";
 
+import red from "../../imgs/red.png";
+import green from "../../imgs/green.png";
+import yellow from "../../imgs/yellow.png";
+
 function Summary() {
   const [data, setData] = useState({
-    labels: ["","Content", "Trainings", "Certification",'Feedback'],
+    labels: ["", "Content", "Trainings", "Certification", "Feedback"],
     datasets: [
       {
         label: "Check badge meaning for performance rating",
         backgroundColor: "#8A1A49",
         borderColor: "#DF5753",
         borderWidth: 1,
-        data: [0,50,30,10,30,100],
+        data: [0, 50, 30, 10, 30, 100],
       },
     ],
   });
@@ -33,40 +37,50 @@ function Summary() {
         </div>
         <div className="summary">
           <div className="summary-graph">
-            {/* <div className="summary-back"> */}
-              <div className="graph">
-                <Line
-                  data={data}
-                  options={{
-                    title: {
-                      display: true,
-                      text: "Performance",
-                      fontSize: 20,
-                    },
-                    legend: {
-                      display: true,
-                      position: "right",
-                    },
-                  }}
-                />
-              </div>
-            {/* </div> */}
+            <div className="graph">
+              <Line
+                data={data}
+                options={{
+                  title: {
+                    display: true,
+                    text: "Performance",
+                    fontSize: 20,
+                  },
+                  legend: {
+                    display: true,
+                    position: "right",
+                  },
+                }}
+              />
+            </div>
             <div className="abilities">
               <h6>Your overall abilities</h6>
               <p></p>
             </div>
           </div>
 
-          <div>
-            <div>
+          <div className="badge">
+            <div className="badge-list">
               <h6>Badge</h6>
               <ul>
-                <li>Average:</li>
-                <li>Good:</li>
-                <li>Exceptional</li>
+                <li>
+                  <img src={red} alt="" />
+                  <span>Average:</span>
+                  <span></span>
+                </li>
+                <li>
+                  <img src={yellow} alt="" />
+                  <span>Good:</span>
+                  <span></span>
+                </li>
+                <li>
+                  <img src={green} alt="" />
+                  <span>Exceptional:</span>
+                  <span></span>
+                </li>
               </ul>
             </div>
-            <div>
+            <div  className="badge-activities">
               <h6>BADGE</h6>
               <ul>
                 <li>Content</li>
